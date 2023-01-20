aec3de7f786754d14cc5299eab82d5a89c5a7cec
ameyaflobiz <ameya@flobiz.in>
2023-01-20 13:23:17 +0530

    initial project setup

---
+++ src/main/java/com/ameyajangam/springboot/meetbooking/MeetBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ameyajangam.springboot.meetbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ameyajangam/springboot/meetbooking/MeetBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ameyajangam.springboot.meetbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
